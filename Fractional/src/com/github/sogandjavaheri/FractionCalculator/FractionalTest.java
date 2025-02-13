1f73b47ec071ac2d3e09a7e0e58b066fdacd6fe9
SogandJavaheri <javaherisogand78@gmail.com>
2025-02-13 13:01:21 +0330

    Delete Fractional/src/com/github/sogandjavaheri directory

--- Fractional/src/com/github/sogandjavaheri/FractionCalculator/FractionalTest.java
+++
@@ -1,9 +0,0 @@
-package com.github.sogandjavaheri.FractionCalculator;
-
-public class FractionalTest {
-    public static void main(String[] args) {
-        Fractional frac = new Fractional();
-        frac.startCalculator();
-    }
-}
-
--- Fractional/src/com/github/sogandjavaheri/Fractional/FractionalTest.java
+++
@@ -1,9 +0,0 @@
-package com.github.sogandjavaheri.Fractional;
-
-public class FractionalTest {
-    public static void main(String[] args) {
-        Fractional frac = new Fractional();
-        frac.startCalculator();
-    }
-}
-
