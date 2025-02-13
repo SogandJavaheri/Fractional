682d18ae68da54754aba305ca042fc42c4513f97
SogandJavaheri <javaherisogand78@gmail.com>
2025-02-13 13:00:08 +0330

    Add files via upload

---
+++ Fractional/src/com/github/sogandjavaheri/Fractional/FractionalTest.java
@@ -0,0 +1,9 @@
+package com.github.sogandjavaheri.Fractional;
+
+public class FractionalTest {
+    public static void main(String[] args) {
+        Fractional frac = new Fractional();
+        frac.startCalculator();
+    }
+}
+
